66e6d3ab00c5efdff5b0949339d655812577b325
yangdaoquan <daoquan.yang@baitaoshuo.com>
2019-06-17 20:07:15 +0800

    init
    init

--- Go/src/simple/main/dup1.go
+++ Go/src/simple/main/dup1.go
@@ -6,12 +6,34 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
+	/*
+		counts := make(map[string]int)
+		input := bufio.NewScanner(os.Stdin)
+		for input.Scan() {
+			counts[input.Text()]++
+		}
+		// NOTE: ignoring potential errors from input.Err()
+		for line, n := range counts {
+			if n > 1 {
+				fmt.Printf("%d\t%s\n", n, line)
+			}
+		}
+	*/
 	counts := make(map[string]int)
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
+		for key := range counts {
+			if strings.EqualFold(input.Text(), key) {
+				fmt.Println("输入有重复字段，原字段为", key, "，冲突字段为：", input.Text(), "，目前总数为：", counts [key])
+			}
+		}
+		if input.Text() == "end" {
+			break
+		}
 		counts[input.Text()]++
 	}
 	// NOTE: ignoring potential errors from input.Err()
