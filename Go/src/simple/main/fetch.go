241431eec63da7c113003792a3477be64b0e85e6
yangdaoquan <daoquan.yang@baitaoshuo.com>
2019-06-25 19:50:47 +0800

    Go语言
    Go语言

--- Go/src/simple/main/fetch.go
+++ Go/src/simple/main/fetch.go
@@ -3,20 +3,47 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
+	/*
+		for _, url := range os.Args[1:] {
+			resp, err := http.Get(url)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+				os.Exit(1)
+			}
+			b, err := ioutil.ReadAll(resp.Body)
+			resp.Body.Close()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+				os.Exit(1)
+			}
+			fmt.Printf("%s", b)
+		}
+	*/
 	for _, url := range os.Args[1:] {
+		if (!strings.HasPrefix(url, "http://")) {
+			fmt.Println("url中缺少前缀http://")
+			url = "http://" + url
+			break;
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+
+		fmt.Println("HTTP协议的状态码为："+resp.Status)
+
+		b, err := io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
+		//b, err := ioutil.ReadAll(resp.Body)
+		//resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
